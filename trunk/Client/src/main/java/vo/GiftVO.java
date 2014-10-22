9c002678c8600a07d7491167651b92a54f68e8f7
winsky <winsky@03a0149d-0379-4f0e-b74e-e84467664baa>
2014-10-22 09:10:26 +0000

    增加了库存部分的几个VO类，删去了我原先配置Jenkins时测试的Demo和YSK文件———严顺宽

--- trunk/Client/src/main/java/vo/GiftVO.java
+++ trunk/Client/src/main/java/vo/GiftVO.java
@@ -1,5 +1,36 @@
 package vo;
 
-public class GiftVO {
+import java.util.Date;
 
+public class GiftVO extends ReceiptVO {
+	GoodsVO goods;
+	int num;
+
+	public GiftVO(MemberVO member, GoodsVO goods, int num, UserVO user,
+			ReceiptType type, String info, String stockid) {
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+//		String timeNow = df.format(new Date());
+		
+		this.member = member;
+		this.goods = goods;
+		this.num = num;
+		this.user = user;
+		this.type = type;
+		this.createDate = new Date();
+		this.status = 0;
+		this.info = info;
+		this.stockid = info;
+	}
+
+	public MemberVO getMember() {
+		return member;
+	}
+
+	public GoodsVO getGoods() {
+		return goods;
+	}
+
+	public int getNum() {
+		return num;
+	}
 }
