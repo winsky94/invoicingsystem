35bbe5dd4a907d5dab175cd4b3acb3e89c3adc3e
winsky94 <13187623+winsky94@users.noreply.github.com>
2014-10-22 09:10:26 +0000

    增加了库存部分的几个VO类，删去了我原先配置Jenkins时测试的Demo和YSK文件———严顺宽

--- Client/src/main/java/vo/GoodsVO.java
+++ Client/src/main/java/vo/GoodsVO.java
@@ -1,5 +1,57 @@
 package vo;
 
 public class GoodsVO {
+	String GoodsID;
+	String name;
+	String size;
+	int numInStock;
+	double purchasePrice;
+	double price;
+	double lastPurchasePrice;
+	double lastPrice;
 
+	public GoodsVO(String goodsID, String name, String size, int numInStock,
+			double purchasePrice, double price, double lastPurchasePrice,
+			double lastPrice) {
+		GoodsID = goodsID;
+		this.name = name;
+		this.size = size;
+		this.numInStock = numInStock;
+		this.purchasePrice = purchasePrice;
+		this.price = price;
+		this.lastPurchasePrice = lastPurchasePrice;
+		this.lastPrice = lastPrice;
+	}
+
+	public String getGoodsID() {
+		return GoodsID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public int getNumInStock() {
+		return numInStock;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public double getLastPurchasePrice() {
+		return lastPurchasePrice;
+	}
+
+	public double getLastPrice() {
+		return lastPrice;
+	}
 }
